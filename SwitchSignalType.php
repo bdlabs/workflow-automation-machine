e906607c13941a6cc1b1f1de9c90a9e828447525
Łukasz Koc <lukasz.koc@rawlplug.com>
2024-06-18 13:33:02 +0200

    Change SwitchSignalType.
    Add toString method.

--- SwitchSignalType.php
+++ SwitchSignalType.php
@@ -2,6 +2,24 @@
 
 namespace DecisionMachine;
 
+use DecisionMachine\FrameWork\SignalType;
+
+/**
+ * Class SwitchSignalType
+ *
+ * @package DecisionMachine
+ */
 class SwitchSignalType extends SignalType
 {
+    /**
+     * @param string $value
+     */
+    public function __construct(private readonly string $value)
+    {
+    }
+
+    public function __toString(): string
+    {
+        return $this::class . '_' . $this->value;
+    }
 }
