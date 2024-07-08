5f0f8f6a0d93ee3af3ce4b63ca5a701f618f1649
Łukasz Koc <lukasz.koc@rawlplug.com>
2024-07-08 23:12:13 +0200

    Add methods types and end empty line.

--- FrameWork/SignalType.php
+++ FrameWork/SignalType.php
@@ -2,8 +2,16 @@
 
 namespace DecisionMachine\FrameWork;
 
+/**
+ * Class SignalType
+ *
+ * @package DecisionMachine\FrameWork
+ */
 class SignalType
 {
+    /**
+     * @return string
+     */
     public function __toString(): string
     {
         return $this::class;
