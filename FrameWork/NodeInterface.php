0d635d2414430b0b1e8929ad55e22bb2fb54ab2e
Łukasz Koc <lukasz.koc@rawlplug.com>
2024-07-08 23:13:02 +0200

    Add methods types and end empty line.

--- FrameWork/NodeInterface.php
+++ FrameWork/NodeInterface.php
@@ -2,11 +2,29 @@
 
 namespace DecisionMachine\FrameWork;
 
+/**
+ * Interface NodeInterface
+ *
+ * @package DecisionMachine\FrameWork
+ */
 interface NodeInterface
 {
+    /**
+     * @param Signal $signal
+     *
+     * @return Signal
+     */
     public function process(Signal $signal): Signal;
 
+    /**
+     * @return array
+     */
     public function expectedSignals(): array;
 
-    public function setEmitter(callable $emitter);
+    /**
+     * @param callable $emitter
+     *
+     * @return void
+     */
+    public function setEmitter(callable $emitter): void;
 }
