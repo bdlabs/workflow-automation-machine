a7bd7ba77be2807bc2b2671e1f1369e8c18e6a29
Łukasz Koc <lukasz.koc@rawlplug.com>
2024-07-08 23:11:53 +0200

    Add methods types.

--- FrameWork/Signal.php
+++ FrameWork/Signal.php
@@ -2,8 +2,18 @@
 
 namespace DecisionMachine\FrameWork;
 
+/**
+ * Class Signal
+ *
+ * @package DecisionMachine\FrameWork
+ */
 class Signal
 {
+    /**
+     * @param NodeSignal $nodeSignal
+     * @param $typeSignal
+     * @param Machine $machine
+     */
     public function __construct(
         private readonly NodeSignal $nodeSignal,
         private $typeSignal,
@@ -11,21 +21,38 @@ class Signal
     ) {
     }
 
+    /**
+     * @return NodeSignal
+     */
     public function signal(): NodeSignal
     {
         return $this->nodeSignal;
     }
 
+    /**
+     * @return string
+     */
     public function type(): string
     {
         return $this->typeSignal::class;
     }
 
+    /**
+     * @param array $data
+     * @param $signalType
+     *
+     * @return Signal
+     */
     public function prepareSignal(array $data, $signalType): Signal
     {
         return $this->machine->prepareSignal($data, $signalType);
     }
 
+    /**
+     * @param string $nodeName
+     *
+     * @return NodeSignal
+     */
     public function getInputs(string $nodeName): NodeSignal
     {
         return $this->machine->getInputs($nodeName)->signal();
