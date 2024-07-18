5adb1ff8d6895e546bc7c99ee8b9d8ad0e833a6b
Łukasz Koc <lukasz.koc@rawlplug.com>
2024-07-18 11:57:38 +0200

    Refactoring, use TreeRender and TreeProcessRunner.

--- src/Machine.php
+++ src/Machine.php
@@ -20,8 +20,6 @@ class Machine
     /** @var Signal[] */
     private array $outputsSignals = [];
 
-    private array $emits = [];
-
     private bool $processStatus = false;
 
     public function __construct()
@@ -32,9 +30,6 @@ class Machine
     }
 
     /**
-     * @param string $nodeName
-     *
-     * @return \DecisionMachine\FrameWork\Signal
      * @throws \Exception
      */
     public function getInputs(string $nodeName): Signal
@@ -71,8 +66,10 @@ class Machine
     {
         $this->processStatus = true;
         $tree = $this->treeRender();
-        $this->emit('start', $inputSignal);
-        $this->emitStart($tree);
+        $this->outputsSignals['start'] = $inputSignal;
+        $this->processStatus = (new TreeProcessRunner($tree, $this->nodeContainer->getAll(), function ($nodeName, $signal) {
+            $this->emit($nodeName, $signal);
+        }))->process($inputSignal);
 
         return $this->signal;
     }
@@ -99,119 +96,6 @@ class Machine
     }
 
     /**
-     * @return array
-     */
-    protected function prepareRelation(): array
-    {
-        return $this->nodeContainer->makeRelation($this->emits);
-    }
-
-    /**
-     * @param TreeNode $tree
-     *
-     * @return void
-     */
-    protected function emitStart(TreeNode $tree): void
-    {
-        $relations = $this->prepareRelation();
-        $this->walkingForTree(
-            $tree,
-            $relations,
-            function (TreeNode $tree) use ($relations) {
-                $sendingNodeName = $tree->parent()->name();
-                $nodeName = $tree->name();
-                $record = array_values(
-                    array_filter(
-                        $relations,
-                        function ($record) use ($nodeName) {
-                            return $record['to'] === $nodeName;
-                        }
-                    )
-                )[0] ?? null;
-                if (!$record) {
-                    return true;
-                }
-                foreach ($record['dependencies'] as $dependency) {
-                    if (!$this->hasInputs($dependency)) {
-                        return false;
-                    }
-                }
-                $signal = $this->getInputs($sendingNodeName);
-                try {
-                    $this->emit($nodeName, $this->nodeContainer->process($nodeName, $signal));
-                } catch (\Throwable $throwable) {
-                    $this->logger->error(
-                        $sendingNodeName,
-                        [
-                            'run' => $nodeName,
-                            'message' => $throwable->getMessage(),
-                        ]
-                    );
-
-                    return -1;
-                }
-
-                return true;
-            }
-        );
-    }
-
-    /**
-     * @param \DecisionMachine\FrameWork\TreeNode $tree
-     * @param array $relations
-     * @param $callBack
-     *
-     * @return void
-     * @throws \Exception
-     */
-    public function walkingForTree(TreeNode $tree, array $relations, $callBack): void
-    {
-        $isDone = true;
-        /** @var TreeNode[] $stack */
-        $stack = [];
-        $stack[] = $tree;
-        while (!empty($stack)) {
-            $stackTmp = [];
-            $currentNode = array_pop($stack);
-            $sendingNodeNameSignal = $this->getInputs($currentNode->name());
-            foreach ($currentNode->lines() as $node) {
-                $nodeName = $node->name();
-                $record = array_values(
-                    array_filter(
-                        $relations,
-                        function ($record) use ($nodeName) {
-                            return $record['to'] === $nodeName;
-                        }
-                    )
-                )[0] ?? ['exception' => new SignalType()];
-                $exceptionSignalType = $record['exception'];
-                if ((!$exceptionSignalType instanceof \Closure && !$sendingNodeNameSignal->signal()->equal($exceptionSignalType)) ||
-                    $exceptionSignalType instanceof \Closure && !$exceptionSignalType($sendingNodeNameSignal)) {
-                    continue;
-                }
-                if ($this->nodeContainer->isExecuted($node->name())) {
-                    $stackTmp[] = $node;
-                    continue;
-                }
-                $this->logger->init($node->name(), json_encode($sendingNodeNameSignal->signal()->valueOf()));
-                $status = $callBack($node);
-                if ($status === -1) {
-                    $this->processStatus = false;
-                    continue;
-                }
-                if ($status) {
-                    $stackTmp[] = $node;
-                }
-                $isDone &= $status;
-            }
-            $stack = array_merge($stack, array_reverse($stackTmp));
-        }
-        if (!$isDone) {
-            $this->walkingForTree($tree, $relations, $callBack);
-        }
-    }
-
-    /**
      * @return bool
      */
     public function status(): bool
@@ -221,10 +105,15 @@ class Machine
 
     protected function emit(string $sendingNodeName, Signal $signal): void
     {
-        $this->emits[] = $sendingNodeName;
         $this->outputsSignals[$sendingNodeName] = $signal;
         $this->signal = $signal;
-        $this->logger->registerOutputSignal($sendingNodeName, ['message' => 'register output signal']);
+        $this->logger->registerOutputSignal(
+            $sendingNodeName,
+            [
+                'message' => 'register output signal',
+                'signal' => $signal->signal()->valueOf(),
+            ]
+        );
     }
 
     /**
@@ -233,43 +122,10 @@ class Machine
      */
     protected function treeRender(): TreeNode
     {
-        $tree = $this->treeRenderStart();
+        $tree = (new TreeRender())->render($this->nodeContainer->getAll());
         $this->treeValidate($tree);
 
         return $tree;
-    }
-
-    /**
-     * @throws \Exception
-     */
-    protected function treeRenderStart(): TreeNode
-    {
-        /** @var SignalType[] $joinedNodesMap */
-        $joinedNodesMap = [];
-        $usedNodes = [];
-        /** @var TreeNode[] $nodes */
-        $nodeList = [];
-        $startNode = new TreeNode('start');
-        $nodeList['start'] = $startNode;
-        foreach ($this->nodeContainer->getAll() as $nodeName => $node) {
-            if ($nodeName !== 'start') {
-                $nodeList[$nodeName] = new TreeNode($nodeName);
-            }
-            $joinedNodesMap[$nodeName] = $node['joined'];
-        }
-
-        foreach ($joinedNodesMap as $signalNodeName => $nodes) {
-            $nodeRoot = &$nodeList[$signalNodeName];
-            foreach ($nodes as $nodeName => $signal) {
-                if (isset($usedNodes[$nodeName])) {
-                    throw new \Exception($nodeName . ' has been used.');
-                }
-                $nodeRoot->join($nodeList[$nodeName]);
-                $usedNodes[$nodeName] = true;
-            }
-        }
-
-        return $startNode;
     }
 
     /**
